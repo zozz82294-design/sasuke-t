19f04f1cd0afa32910cdc526aab8d16511dc8452
zozz82294-design <zozz82294@gmail.com>
2026-05-13 11:11:06 -1000

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const http = require("http");
 const fs = require("fs");
-const path = require("path");
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  let filePath = path.join(__dirname, "index.html");
-
-  fs.readFile(filePath, (err, content) => {
+  fs.readFile("./index.html", (err, data) => {
     if (err) {
       res.writeHead(500);
       res.end("Error loading file");
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(content);
+      return;
     }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
   });
 });
 
