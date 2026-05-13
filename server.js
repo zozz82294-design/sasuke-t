75c9680d40eda6861a25f9dc2da312df0794ae88
zozz82294-design <zozz82294@gmail.com>
2026-05-13 11:43:38 -1000

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,25 @@
-const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
+const fs = require("fs");
+const path = require("path");
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+const server = http.createServer((req, res) => {
 
-let room = {
-  id: null,
-  players: [],
-  host: null
-};
+  let filePath = path.join(__dirname, "public", "index.html");
 
-function generateRoomId() {
-  return Math.random().toString(36).substring(2, 7);
-}
-
-io.on("connection", (socket) => {
-
-  socket.on("createRoom", (name) => {
-    room.id = generateRoomId();
-    room.players = [];
-    room.host = socket.id;
-
-    socket.join(room.id);
-
-    room.players.push({ id: socket.id, name: "SASUKE", host: true });
-
-    socket.emit("roomCreated", room.id);
-    io.to(room.id).emit("updatePlayers", room.players);
-  });
-
-  socket.on("joinRoom", ({ roomId, name }) => {
-    if (room.id !== roomId) {
-      socket.emit("errorMsg", "لقد انتهت صلاحية الرابط");
-      return;
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Error loading file");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.end(content);
     }
-
-    socket.join(room.id);
-
-    room.players.push({ id: socket.id, name, host: false });
-
-    io.to(room.id).emit("updatePlayers", room.players);
-  });
-
-  socket.on("disconnect", () => {
-    room.players = room.players.filter(p => p.id !== socket.id);
-    io.to(room.id).emit("updatePlayers", room.players);
   });
 
 });
 
-server.listen(process.env.PORT || 3000, "0.0.0.0", () => {
-  console.log("Server running");
+server.listen(port, () => {
+  console.log("Server running on port " + port);
 });
