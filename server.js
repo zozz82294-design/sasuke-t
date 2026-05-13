2389f0cc6fed193e0d392e260050212bc0cc1ac1
zozz82294-design <zozz82294@gmail.com>
2026-05-13 11:35:11 -1000

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,58 @@
+const express = require("express");
 const http = require("http");
-const fs = require("fs");
+const { Server } = require("socket.io");
 
-const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-const server = http.createServer((req, res) => {
-  fs.readFile("index.html", (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      res.end("Error loading file");
+app.use(express.static("public"));
+
+let room = {
+  id: null,
+  players: [],
+  host: null
+};
+
+function generateRoomId() {
+  return Math.random().toString(36).substring(2, 7);
+}
+
+io.on("connection", (socket) => {
+
+  socket.on("createRoom", (name) => {
+    room.id = generateRoomId();
+    room.players = [];
+    room.host = socket.id;
+
+    socket.join(room.id);
+
+    room.players.push({ id: socket.id, name: "SASUKE", host: true });
+
+    socket.emit("roomCreated", room.id);
+    io.to(room.id).emit("updatePlayers", room.players);
+  });
+
+  socket.on("joinRoom", ({ roomId, name }) => {
+    if (room.id !== roomId) {
+      socket.emit("errorMsg", "لقد انتهت صلاحية الرابط");
       return;
     }
 
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
+    socket.join(room.id);
+
+    room.players.push({ id: socket.id, name, host: false });
+
+    io.to(room.id).emit("updatePlayers", room.players);
   });
+
+  socket.on("disconnect", () => {
+    room.players = room.players.filter(p => p.id !== socket.id);
+    io.to(room.id).emit("updatePlayers", room.players);
+  });
+
 });
 
-server.listen(port, "0.0.0.0", () => {
-  console.log("Server running on port " + port);
+server.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+  console.log("Server running");
 });
