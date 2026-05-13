bc10f8b90750252ae869a70eb74990a6aa2a3b32
zozz82294-design <zozz82294@gmail.com>
2026-05-14 01:25:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,16 +7,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// تقديم ملفات public
 app.use(express.static(path.join(__dirname, "public")));
 
 let rooms = {};
 
 io.on("connection", (socket) => {
 
-  // 🔥 إنشاء غرفة
   socket.on("createRoom", (name) => {
-
     const roomId = Math.floor(1000 + Math.random() * 9000).toString();
 
     rooms[roomId] = [];
@@ -33,18 +30,16 @@ io.on("connection", (socket) => {
     io.to(roomId).emit("updatePlayers", rooms[roomId]);
   });
 
-  // 🔥 دخول غرفة من اللينك
   socket.on("joinRoom", ({ roomId, name }) => {
 
     if (!rooms[roomId]) {
-      socket.emit("errorMsg", "لقد انتهت صلاحية الرابط");
+      socket.emit("errorMsg", "انتهت صلاحية الرابط");
       return;
     }
 
-    // منع تكرار الاسم
     const exists = rooms[roomId].find(p => p.name === name);
     if (exists) {
-      socket.emit("errorMsg", "الاسم مستخدم بالفعل");
+      socket.emit("errorMsg", "الاسم مستخدم");
       return;
     }
 
@@ -59,9 +54,7 @@ io.on("connection", (socket) => {
     io.to(roomId).emit("updatePlayers", rooms[roomId]);
   });
 
-  // 🔥 الخروج
   socket.on("disconnect", () => {
-
     for (let roomId in rooms) {
       rooms[roomId] = rooms[roomId].filter(p => p.id !== socket.id);
 
@@ -71,12 +64,10 @@ io.on("connection", (socket) => {
         io.to(roomId).emit("updatePlayers", rooms[roomId]);
       }
     }
-
   });
 
 });
 
-// بورت Railway
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
