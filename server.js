a8194b76b958cbb9173ceffaae365d5e5987f452
zozz82294-design <zozz82294@gmail.com>
2026-05-14 06:22:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ let room = {
   started: false
 };
 
+const categories = {
+  animals: ["أسد","نمر","فيل","زرافة","قرد","كلب","قط","حصان","ذئب"],
+  objects: ["تلاجة","مروحة","سرير","كنبة","معلقة","كوباية","خلاط","بامبرز"],
+  apps: ["واتساب","فيسبوك","يوتيوب","تيك توك","انستجرام"],
+  anime: ["ناروتو","ون بيس","ديث نوت","هجوم العمالقة"],
+  cartoon: ["سبونج بوب","توم وجيري","بن تن"]
+};
+
 io.on("connection", (socket) => {
 
   socket.on("createRoom", (name) => {
@@ -25,7 +33,6 @@ io.on("connection", (socket) => {
     room.started = false;
 
     room.players.push({ id: socket.id, name });
-
     io.emit("updatePlayers", room.players);
   });
 
@@ -34,7 +41,40 @@ io.on("connection", (socket) => {
     io.emit("updatePlayers", room.players);
   });
 
-  // بدء التصويت
+  socket.on("startGame", () => {
+    room.started = true;
+    io.emit("showCategories");
+  });
+
+  socket.on("chooseCategory", (cat) => {
+    room.category = cat;
+
+    const words = categories[cat];
+    room.word = words[Math.floor(Math.random() * words.length)];
+
+    const others = room.players.slice(1);
+    const spy = others[Math.floor(Math.random() * others.length)];
+    room.spyId = spy.id;
+
+    io.emit("categoryChosen", cat);
+
+    room.players.forEach(p => {
+      if (p.id === room.spyId) {
+        io.to(p.id).emit("yourRole", {
+          spy: true,
+          category: cat
+        });
+      } else {
+        io.to(p.id).emit("yourRole", {
+          spy: false,
+          word: room.word,
+          category: cat
+        });
+      }
+    });
+  });
+
+  // 🔥 التصويت
   socket.on("startVote", () => {
     room.votes = {};
     room.voted = {};
@@ -46,10 +86,8 @@ io.on("connection", (socket) => {
     io.emit("voteStarted", room.players);
   });
 
-  // التصويت
   socket.on("vote", ({ voter, targetId }) => {
-
-    if (room.voted[voter]) return; // منع تكرار
+    if (room.voted[voter]) return;
 
     room.voted[voter] = true;
     room.votes[targetId].push(voter);
@@ -60,24 +98,59 @@ io.on("connection", (socket) => {
       max: room.players.length
     });
 
-    // لو الكل صوت
     if (Object.keys(room.voted).length >= room.players.length) {
 
-      let maxVotes = 0;
+      let max = 0;
       let selected = null;
 
       for (let id in room.votes) {
-        if (room.votes[id].length > maxVotes) {
-          maxVotes = room.votes[id].length;
+        if (room.votes[id].length > max) {
+          max = room.votes[id].length;
           selected = id;
         }
       }
 
-      io.emit("voteResult", selected);
+      io.emit("voteResult", {
+        selected,
+        spy: room.spyId,
+        players: room.players
+      });
     }
   });
 
-  // خروج لاعب بدون جلتش
+  // 🔥 التخمين
+  socket.on("startGuess", () => {
+    let words = [room.word];
+
+    const all = [
+      "أسد","نمر","فيل","زرافة","قرد","كلب","قط","حصان",
+      "ذئب","ثعلب","دب","خروف","بقرة","جمل"
+    ];
+
+    while (words.length < 13) {
+      const w = all[Math.floor(Math.random() * all.length)];
+      if (!words.includes(w)) words.push(w);
+    }
+
+    words.sort(() => Math.random() - 0.5);
+
+    io.emit("showGuess", {
+      words,
+      spyId: room.spyId
+    });
+  });
+
+  socket.on("selectGuess", ({ word, player }) => {
+    io.emit("playerSelected", { word, player });
+  });
+
+  socket.on("confirmGuess", (word) => {
+    io.emit("guessResult", {
+      word,
+      correct: room.word
+    });
+  });
+
   socket.on("disconnect", () => {
     room.players = room.players.filter(p => p.id !== socket.id);
 
@@ -93,5 +166,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log("🔥 Voting Server Ready");
+  console.log("🔥 Server Ready");
 });
