a738fc3209eb8f774dce31296b2d6816d3e9a249
zozz82294-design <zozz82294@gmail.com>
2026-05-14 05:51:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
 let players = [];
+let roomId = null;
 
 io.on("connection", (socket) => {
 
+  // إنشاء غرفة
   socket.on("createRoom", (name) => {
     players = [];
+    roomId = Math.floor(Math.random() * 9999);
+
     players.push({ id: socket.id, name });
 
-    socket.emit("updatePlayers", players);
+    socket.emit("roomCreated", roomId);
+    io.emit("updatePlayers", players);
   });
 
-  socket.on("joinRoom", ({ name }) => {
+  // انضمام
+  socket.on("joinRoom", ({ roomId: rid, name }) => {
+    if (rid != roomId) {
+      socket.emit("errorMsg", "الرابط انتهت صلاحيته");
+      return;
+    }
+
     players.push({ id: socket.id, name });
 
     io.emit("updatePlayers", players);
@@ -23,16 +44,20 @@ io.on("connection", (socket) => {
     io.emit("updatePlayers", players);
   });
 
-  // طرد لاعب
+  // طرد
   socket.on("kickPlayer", (id) => {
     players = players.filter(p => p.id !== id);
-
     io.emit("updatePlayers", players);
   });
 
-  // إعادة اللعبة
-  socket.on("restartGame", () => {
-    io.emit("gameRestarted");
+  // مغادرة (لما يقفل أو يخرج)
+  socket.on("disconnect", () => {
+    players = players.filter(p => p.id !== socket.id);
+    io.emit("updatePlayers", players);
   });
 
+});
+
+server.listen(3000, () => {
+  console.log("Server running...");
 });
