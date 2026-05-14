558a09659b2d383220294d550c28417c16d472c8
zozz82294-design <zozz82294@gmail.com>
2026-05-14 05:47:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,75 +1,38 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static(path.join(__dirname, "public")));
-
-let rooms = {};
+let players = [];
 
 io.on("connection", (socket) => {
 
   socket.on("createRoom", (name) => {
-    const roomId = Math.floor(1000 + Math.random() * 9000).toString();
+    players = [];
+    players.push({ id: socket.id, name });
 
-    rooms[roomId] = [];
-
-    socket.join(roomId);
-
-    rooms[roomId].push({
-      id: socket.id,
-      name: name,
-      host: true
-    });
-
-    socket.emit("roomCreated", roomId);
-    io.to(roomId).emit("updatePlayers", rooms[roomId]);
+    socket.emit("updatePlayers", players);
   });
 
-  socket.on("joinRoom", ({ roomId, name }) => {
+  socket.on("joinRoom", ({ name }) => {
+    players.push({ id: socket.id, name });
 
-    if (!rooms[roomId]) {
-      socket.emit("errorMsg", "انتهت صلاحية الرابط");
-      return;
-    }
-
-    const exists = rooms[roomId].find(p => p.name === name);
-    if (exists) {
-      socket.emit("errorMsg", "الاسم مستخدم");
-      return;
-    }
-
-    socket.join(roomId);
-
-    rooms[roomId].push({
-      id: socket.id,
-      name: name,
-      host: false
-    });
-
-    io.to(roomId).emit("updatePlayers", rooms[roomId]);
+    io.emit("updatePlayers", players);
   });
 
-  socket.on("disconnect", () => {
-    for (let roomId in rooms) {
-      rooms[roomId] = rooms[roomId].filter(p => p.id !== socket.id);
+  // تغيير اسم
+  socket.on("renamePlayer", ({ id, newName }) => {
+    const player = players.find(p => p.id === id);
+    if (player) player.name = newName;
 
-      if (rooms[roomId].length === 0) {
-        delete rooms[roomId];
-      } else {
-        io.to(roomId).emit("updatePlayers", rooms[roomId]);
-      }
-    }
+    io.emit("updatePlayers", players);
   });
 
-});
+  // طرد لاعب
+  socket.on("kickPlayer", (id) => {
+    players = players.filter(p => p.id !== id);
 
-const PORT = process.env.PORT || 3000;
+    io.emit("updatePlayers", players);
+  });
 
-server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  // إعادة اللعبة
+  socket.on("restartGame", () => {
+    io.emit("gameRestarted");
+  });
+
 });
